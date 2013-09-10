1d7c6723d4e5742743e358563cef302719ab5e6e
Steven Hubert <steven.hubert@gmail.com>
2013-09-10 16:40:19 -0600

    Added Congress API, Refactors the zipcode sanitizer

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,7 @@
 require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "43099cda060647608f76e966f3119679"
 
 # def clean_zipcode(zipcode)
 # 	if zipcode.nil?
@@ -15,7 +18,17 @@ require "csv"
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5, "0")[0..4]
-	end			
+	end		
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+		legislator_names = legislators.collect do |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	end
+
+	legislators_string = legislator_names.join(", ")
+end
+
 			
 
 puts "Everything Intialized"
@@ -44,11 +57,16 @@ puts "Everything Intialized"
 # end
 
 
+
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
 	name = row[:first_name]
-	zipcode = row[:zipcode]
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = legislators_by_zipcode(zipcode)
 	
 
-	puts "#{name} #{zipcode}"
+	puts "#{name} #{zipcode} #{legislators}"
+
 end
+
+
